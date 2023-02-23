e95052fe39c9fa89e610655afa026ee1c5ff0b2d
Juan Feris <feris.juan@gmail.com>
2023-02-23 10:15:34 -0500

    add city list

---
+++ src/components/CityList.vue
@@ -0,0 +1,54 @@
+<template>
+  <div v-for="city in savedCities" :key="city.id">
+    <CityCard :city="city" @click="goToCityView(city)" />
+  </div>
+
+  <p v-if="savedCities.length === 0">
+    No locations added. To start tracking a location, search in the field above.
+  </p>
+</template>
+
+<script setup>
+import axios from "axios";
+import {ref} from "vue";
+import {useRouter} from "vue-router";
+import CityCard from "./CityCard.vue";
+
+const savedCities = ref([]);
+
+const getCities = async () => {
+  const SAVED_CITIES_KEY = "savedCities";
+  const localStorageSavedCities = localStorage.getItem(SAVED_CITIES_KEY);
+
+  if (localStorageSavedCities) {
+    savedCities.value = JSON.parse(localStorageSavedCities);
+
+    const requests = [];
+    savedCities.value.forEach((city) => {
+      const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+      url.searchParams.set("lat", city.coords.lat);
+      url.searchParams.set("lon", city.coords.lon);
+      url.searchParams.set("appid", import.meta.env.VITE_OPEN_WEATHER_API_KEY);
+      url.searchParams.set("units", "metric");
+      requests.push(axios.get(url));
+    });
+
+    const weatherData = await Promise.all(requests);
+
+    weatherData.forEach((value, index) => {
+      savedCities.value[index].weather = value.data;
+    });
+  }
+};
+
+await getCities();
+
+const router = useRouter();
+const goToCityView = (city) => {
+  router.push({
+    name: "cityView",
+    params: {state: city.state, city: city.city},
+    query: {lat: city.coords.lat, lon: city.coords.lon},
+  });
+};
+</script>
---
+++ src/views/AsyncCityView.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex flex-col flex-1 items-center">
+    <div
+      v-if="query.preview"
+      class="text-white p-4 bg-weather-secondary w-full text-cet"
+    >
+      <p class="text-center">
+        You are currenly previewing this city, click the "+" icon to start
+        tracking this city.
+      </p>
+    </div>
+
+    <div class="flex flex-col items-center text-white py-12">
+      <h1 class="text-4xl mb-2">{{ params.city }}</h1>
+      <p class="text-sm mb-12">
+        {{
+          new Date(weatherData.currentTime).toLocaleDateString("en-us", {
+            weekday: "short",
+            day: "2-digit",
+            month: "long",
+          })
+        }}
+      </p>
+      <p class="text-8xl mb-8">{{ Math.round(weatherData.main.temp) }}&deg;</p>
+      <p>Feels like {{ Math.round(weatherData.main.feels_like) }}&deg;</p>
+      <p class="capitalize">{{ weatherData.weather[0].description }}</p>
+      <img
+        class="w-[150px] h-auto"
+        :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import axios from "axios";
+import {useRoute} from "vue-router";
+
+const {params, query} = useRoute();
+
+const getWeatherData = async () => {
+  try {
+    const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+
+    url.searchParams.set("lat", query.lat);
+    url.searchParams.set("lon", query.lon);
+    url.searchParams.set("appid", import.meta.env.VITE_OPEN_WEATHER_API_KEY);
+    url.searchParams.set("units", "metric");
+
+    const weatherData = await axios.get(url.href);
+
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = weatherData.data.dt * 1000 + localOffset;
+
+    weatherData.data.currentTime = utc + 1000 * weatherData.data.timezone;
+
+    return weatherData.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const weatherData = await getWeatherData();
+</script>
