5da87288c96ebddf86084214cc97093c434d3dc6
Juan Feris <feris.juan@gmail.com>
2023-02-23 13:48:45 -0500

    [wip] transitions

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <section class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
-    <RouterView />
+    <RouterView v-slot="{Component}">
+      <Transition name="page" mode="out-in">
+        <component :is="Component"/>
+      </Transition>
+    </RouterView>
   </section>
 </template>
 
 <script setup>
+import {Transition} from 'vue';
 import {RouterView} from "vue-router";
 import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
